e454ff41fb5a95002ceaa5282f62f68930066b43
daohd2003 <daoha20102003@gmail.com>
2025-05-15 07:36:00 +0700

    Validate email and password

--- DTOs/Request/LoginRequestDto.cs
+++ DTOs/Request/LoginRequestDto.cs
@@ -1,8 +1,12 @@
-﻿namespace LibraryManagement.DTOs.Request
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagement.DTOs.Request
 {
     public class LoginRequestDto
     {
-        public string Email { get; set; }
-        public string Password { get; set; }
+        [EmailAddress(ErrorMessage = "Email format is invalid.")]
+        public string Email { get; set; } = String.Empty;
+        [MinLength(6)]
+        public string Password { get; set; } = String.Empty;
     }
 }
